3e5ddb6cc718b101a8ecb0463c1daf12c55f23f0
Divan Visagie <me@divanv.com>
2023-05-30 21:24:23 +0200

    Update cgip.rb

--- cgip.rb
+++ cgip.rb
@@ -2,7 +2,7 @@
 class Cgip < Formula
   desc "Terminal client for Chat GPT"
   homepage "https://github.com/divanvisagie/chat-gipity"
-  url "https://github.com/divanvisagie/chat-gipity/releases/download/v0.1.2/cgip"
+  url "https://github.com/divanvisagie/chat-gipity/releases/download/v0.1.21/cgip"
   sha256 "5fcb9b250efb7d32bae15e1b6b252d4f261beffd1ff344c428cf761a441500f5"
 
   def install
