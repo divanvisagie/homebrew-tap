be60547562e2b9befecb4528acbb56ac4dd73601
Divan Visagie <me@divanv.com>
2024-05-17 13:43:02 +0200

    Update cgip.rb

--- cgip.rb
+++ cgip.rb
@@ -8,7 +8,7 @@ class Cgip < Formula
     sha256 "b2c7a676a13ec460ea72e47cd2b34c2858fe662012a479569916a11081366cba"
   elsif Hardware::CPU.arm?
     url "https://github.com/divanvisagie/chat-gipity/releases/download/v0.2.5/cgip-darwin-arm64.tar.gz"
-    sha256 "7683b99d1c85e058628139164a0e0fcf4879c00470eeb0ebfd00b966e3ec1ac5"
+    sha256 "6f78aed6a1a4f731924e7b7f3af72256a913238b68b0a5bc23724fc62e5d7bb8"
   else
     odie "Your architecture is not supported!"
   end
