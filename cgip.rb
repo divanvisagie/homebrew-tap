0e2d9ff0dc34d05fe08469bd1dbb9fbb8a24f58f
Divan Visagie <me@divanv.com>
2024-05-26 10:27:06 +0200

    Update cgip.rb

--- cgip.rb
+++ cgip.rb
@@ -5,7 +5,7 @@ class Cgip < Formula
   # Dynamically set the URL and SHA256 based on the CPU architecture
   if Hardware::CPU.intel?
     url "https://github.com/divanvisagie/chat-gipity/releases/download/v0.2.6/cgip-darwin-x86_64.tar.gz"
-    sha256 "b2c7a676a13ec460ea72e47cd2b34c2858fe662012a479569916a11081366cba"
+    sha256 "c138043a4a1d226b46527425fdad9da7ee62369eb3c9e9273a44911ea520fc6b"
   elsif Hardware::CPU.arm?
     url "https://github.com/divanvisagie/chat-gipity/releases/download/v0.2.6/cgip-darwin-arm64.tar.gz"
     sha256 "c30564aca93d1a9f78ba34f823a8afd215c4f626ca045f5ac42214d2f80d787a"
