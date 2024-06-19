e449da3af293e65b1d5508edc5bddfedc0b0e8eb
Divan Visagie <me@divanv.com>
2024-06-20 00:11:13 +0200

    Fix hashes

--- cgip.rb
+++ cgip.rb
@@ -5,10 +5,10 @@ class Cgip < Formula
   # Dynamically set the URL and SHA256 based on the CPU architecture
   if Hardware::CPU.intel?
     url "https://github.com/divanvisagie/chat-gipity/releases/download/v0.2.9/cgip-darwin-x86_64.tar.gz"
-    sha256 "b9daf109068207cae342b0841f1e24362edc1cff5ddaf2ae5620601cd638413e"
+    sha256 "9c319de122e281355a73651785ab1f4409a66154944b0451470e4880eb4e8119"
   elsif Hardware::CPU.arm?
     url "https://github.com/divanvisagie/chat-gipity/releases/download/v0.2.9/cgip-darwin-aarch64.tar.gz"
-    sha256 "a449aacaddd247020896a6f103cf0038bb85dd3050ceb4357cd79706f4580e86"
+    sha256 "bd49e108bc0e41b50f614db74d4c54cf1f33c188ab51ca2af730ef5d1a033f9a"
   else
     odie "Your architecture is not supported!"
   end
