c69acdb9c7ded7b4d4f57dfc49313a244c2c0b8a
Divan Visagie <me@divanv.com>
2024-06-20 21:15:47 +0200

    Again

--- nocol.rb
+++ nocol.rb
@@ -5,10 +5,10 @@ class Nocol < Formula
   # Dynamically set the URL and SHA256 based on the CPU architecture
   if Hardware::CPU.intel?
     url "https://github.com/divanvisagie/nocol/releases/download/test-release/nocol-darwin-x86_64.tar.gz"
-    sha256 "f851695451a73031b01ade8ac2bd6ac6b9646308fc0c5a97c4a3b3fb528fe515"
+    sha256 "0794a96ab8b943b6b16eb73ac14a1d3765e9a762fd0f812e9bd4cd799cfc0bd7"
   elsif Hardware::CPU.arm?
     url "https://github.com/divanvisagie/nocol/releases/download/test-release/nocol-darwin-aarch64.tar.gz"
-    sha256 "b4c3ab7f314bf73d84f011ba5d44fa0be3a390126cc52c74ccbe834d96de1bcb"
+    sha256 "fe469ea3776b9a824e8b9bbac3e7da15ec688c4cf5307840e09d9e30bcfc360d"
   else
     odie "Your architecture is not supported!"
   end
