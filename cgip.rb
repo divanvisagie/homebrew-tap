9ea229dfc461a71fd0e7a68112c65deabf2f20b0
Divan Visagie <me@divanv.com>
2025-05-29 10:10:35 +0200

    feat: Update CGIP release URL and SHA256 for v0.4.0
    This commit updates the release URL and SHA256 hash for the CGIP binary,
    reflecting the v0.4.0 release. This ensures correct dependency resolution
    and builds.

--- cgip.rb
+++ cgip.rb
@@ -4,11 +4,11 @@ class Cgip < Formula
   
   # Dynamically set the URL and SHA256 based on the CPU architecture
   if Hardware::CPU.intel?
-    url "https://github.com/divanvisagie/chat-gipity/releases/download/v0.3.0/cgip-darwin-x86_64.tar.gz"
-    sha256 "61fe112cb25c8e9afb16a8194df7c0237912c855c1050cc0d8c3a2a578fa5aae"
+    url "https://github.com/divanvisagie/chat-gipity/releases/download/v0.4.0/cgip-darwin-x86_64.tar.gz"
+    sha256 "f73a21e71c62e5692bc521d78edf2d119358d91ef33359cf01213c520926505b"
   elsif Hardware::CPU.arm?
-    url "https://github.com/divanvisagie/chat-gipity/releases/download/v0.3.0/cgip-darwin-aarch64.tar.gz"
-    sha256 "064aba28eefc42851097967173199f3e2a55f2a5778c8372f9223800ce0d3e74"
+    url "https://github.com/divanvisagie/chat-gipity/releases/download/v0.4.0/cgip-darwin-aarch64.tar.gz"
+    sha256 "236100300614af2e73c1f6fee8711d8de1c6955b7902f088017912546f46ea5e"
   else
     odie "Your architecture is not supported!"
   end
